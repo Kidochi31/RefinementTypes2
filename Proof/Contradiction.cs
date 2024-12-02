602c1169a2673ac0f88e62128c134564f8f71342
Kidochi31 <jasonmichaelsinclair@gmail.com>
2024-12-02 23:06:07 +1000

    Adding general imposition logic. Contradiction stubs added.

---
+++ Proof/Contradiction.cs
@@ -0,0 +1,25 @@
+﻿using RefinementTypes2.StandardTyping;
+using RefinementTypes2.Typing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefinementTypes2.Proof
+{
+    internal static class Contradiction
+    {
+        public static bool TypeSelfContradicts(StandardType type, Context context)
+            => type.All(product => RefinementProductAContradictsB(product, product, context));
+
+
+        public static bool RefinementProductAContradictsB(RefinementProduct a, RefinementProduct b, Context context)
+            => a.Any(refinementA => b.Any(refinementB => RefinementAContradictsB(refinementA, refinementB, context)));
+
+        public static bool RefinementAContradictsB(Refinement.Standard a, Refinement.Standard b, Context context)
+        {
+            return false;
+        }
+    }
+}
---
+++ Proof/Imposition.cs
@@ -0,0 +1,29 @@
+﻿using RefinementTypes2.StandardTyping;
+using RefinementTypes2.Typing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefinementTypes2.Proof
+{
+    internal static class Imposition
+    {
+        public static bool ImposeBOnA(StandardType a, StandardType b)
+        {
+            // A fits into B iff A U (not B) = 0
+            StandardType notB = StandardType.Invert(b);
+
+            Context context = new Context();
+
+            if (Contradiction.TypeSelfContradicts(b, context))
+                return false;
+
+            // Here, if B = (a b) | (c d), then (not B) = (a' | b') & (c' | d') = a'c' | a'd' | b'c' | b'd'
+            // It must be shown that A & (not B) is a contradiction on all of these, AND on all cases of A
+            // So, algorithm is to go through all products of A, and all products of inverted B, and check for contradiction.
+            return a.All(sopA => notB.All(sopB => Contradiction.RefinementProductAContradictsB(sopA, sopB, context)));
+        }
+    }
+}
